d18136f3cac257f5d9f4483225c2b527a35287e6
Artem Kurzanov <kurzanovart@gmail.com>
2022-12-14 22:54:04 +0500

    Create f_sales.sql

---
+++ f_sales.sql
@@ -0,0 +1,15 @@
+INSERT INTO mart.f_sales (date_id, item_id, customer_id, city_id, quantity, payment_amount, status)
+SELECT 
+    dc.date_id,
+    item_id,
+    customer_id,
+    city_id,
+    quantity,
+    CASE 
+        WHEN status = 'refunded'  THEN payment_amount * (-1)
+        ELSE else payment_amount
+    END AS payment_amount, 
+    status
+FROM staging.user_order_log uol
+LEFT JOIN mart.d_calendar AS dc ON uol.date_time::Date = dc.date_actual
+WHERE uol.date_time::Date = '{{ds}}';
